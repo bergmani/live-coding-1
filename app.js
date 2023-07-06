8131633f40c54c5f21cff88d4d515febbf247633
bergmani <bergman.ijp@gmail.com>
2023-07-06 12:39:48 -0600

    se realiza la actividad live codig

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Escribe tu código aquí.
+const lista = document.getElementById("lista");
+
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+
+const Palabras = (palabra, palabras) => {
+    let letrasPalabra = 0;
+    for(let i = 0; i < palabra.length ; i ++){
+        letrasPalabra ++;
+    }
+    return letrasPalabra;
+
+console.log(letrasPalabra)
+}
+console.log(Palabras('bocina', myArray));
+
+
+
+
+const li = document.createElement('li');
+li.textContent = Palabras('bocina', myArray);
+lista.appendChild(li);
